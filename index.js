5c6c7a06efb5f21876d618ee1d6a296d47a79933
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-04-21 15:38:00 -0400

    able to delete the .ptl extension using a recursive function to look through nested folders

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const homedir = require("os").homedir();
 const inquirer = require("inquirer");
 
+let mainFilePath = "";
+
 // Show the directory prompt
 inquirer
   .prompt([
@@ -25,62 +27,76 @@ inquirer
   ])
   .then((answers) => {
     // answers.directoryPath contains the selected directory path
-    const selectedDirectoryPath = answers.directoryPath;
+    const directoryPath = answers.directoryPath;
     const location = answers.location;
-    console.log("Selected directory:", selectedDirectoryPath);
-    // Use the selected directory path for further operations
-    // ...
-    readFilesInDirectory(location, selectedDirectoryPath);
+    console.log("Selected directory:", directoryPath);
+    mainFilePath = path.join(homedir, location, directoryPath);
+    console.log("filePath", mainFilePath);
+    // readFilesInDirectory(location, selectedDirectoryPath);
+    findFilesWithExtension(mainFilePath, ".ptl");
   })
   .catch((err) => {
     console.error(err);
   });
 
-// console.log("home directory", homedir);
-// console.log(
-//   "extension test in homedirectory",
-//   path.join(homedir, "/Users/andrewkleindienst/desktop", directory)
-// );
+// Function to find files with a particular extension
+function findFilesWithExtension(startPath, extension) {
+  console.log("in findFilesWithExtension function");
+  // Check if the start path is a directory
+  if (!fs.existsSync(startPath) || !fs.lstatSync(startPath).isDirectory()) {
+    throw new Error("Provided start path is not a valid directory");
+  }
 
-function readFilesInDirectory(location, directoryPath) {
-  //   reads the files in the directory specified
-  const filePath = path.join(homedir, location, directoryPath);
-  console.log("This is filePath", filePath);
+  // Array to store matching files
+  const matchingFiles = [];
 
-  fs.readdir(filePath, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory: ${err}`);
-      return;
-    }
-    // Filters the files in the directory to only include the ones that have '.ptl' extension
-    const newFiles = files.filter((file) => /\.ptl$/.test(file));
-    console.log("This is newFiles", newFiles);
-    // interates over each file in the array of newFiles and runs removeExtension()
-    newFiles.forEach((file) => {
-      removeExtension(location, directoryPath, file);
+  // Recursive function to search for files with the desired extension
+  async function findFilesRecursively(dir) {
+    console.log("in findFilesRecursively function");
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      console.log("file", file);
+      const filePath = path.join(dir, file);
+      console.log("FilePath", filePath);
+      const stat = fs.lstatSync(filePath);
+      if (stat.isDirectory()) {
+        findFilesRecursively(filePath); // Recursively search subdirectories
+      } else if (path.extname(file) === extension) {
+        matchingFiles.push(filePath); // Add matching file to the array
+        console.log("matchingFiles", matchingFiles);
+        removeExtension(matchingFiles);
+      }
     });
-  });
+  }
+
+  findFilesRecursively(startPath); // Start the recursive search
+  //   console.log("matchingFiles", matchingFiles);
+
+  return matchingFiles;
 }
 
-const removeExtension = (location, directoryPath, fileName) => {
-  //   Identifies wha the extension name is for the file
-  const fileExtension = path.extname(fileName);
-  //   removes the file extension
-  const newFileName = fileName.replace(fileExtension, "");
-  console.log("in removeExtension", fileExtension, newFileName);
-  //   runs the rename function using the old file name and the new file name
-  renameFile(location, directoryPath, fileName, newFileName);
+const removeExtension = async (matchingFiles) => {
+  console.log("in removeExtension function");
+  matchingFiles.forEach((file) => {
+    //   Identifies wha the extension name is for the file
+    const fileExtension = path.extname(file);
+    //   removes the file extension
+    const newFileName = file.replace(fileExtension, "");
+    console.log("in removeExtension", fileExtension, newFileName);
+    //   runs the rename function using the old file name and the new file name
+    renameFile(file, newFileName);
+  });
 };
 
-const renameFile = (location, directoryPath, fileName, newFileName) => {
+const renameFile = (fileName, newFileName) => {
   //   rename function using fs()
   fs.rename(fileName, newFileName, async () => {
     console.log("This is fileName", fileName);
     console.log("This is the new file name", newFileName);
   });
   //   The 'directory' should probably be passed in as a parameter
-  const oldFilePath = path.join(homedir, location, directoryPath, fileName); // specify the current file path
-  const newFilePath = path.join(homedir, location, directoryPath, newFileName); // specify the new file path
+  const oldFilePath = path.join(fileName); // specify the current file path
+  const newFilePath = path.join(newFileName); // specify the new file path
 
   fs.rename(oldFilePath, newFilePath, (err) => {
     if (err) {
@@ -92,5 +108,7 @@ const renameFile = (location, directoryPath, fileName, newFileName) => {
     }
   });
 };
+
+// findFilesWithExtension(mainFilePath, ".ptl");
 
 // readFilesInDirectory(directory);
