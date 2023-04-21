fb7e538ddabac4f0b2c3aa03f93199833f6a175b
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-04-21 10:06:55 -0400

    added filter using regex to only choose files that have '.ptl' extension

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 const fs = require("fs");
 const path = require("path");
+const homedir = require("os").homedir();
+
+const directory = "/Extension-Test";
+
+console.log("home directory", homedir);
+console.log(
+  "extension test in homedirectory",
+  path.join(homedir, "/Users/andrewkleindienst/desktop", directory)
+);
 
 // Just using the extname() will find the last extension.
 // Probably needs to be some conditional logic to tell it to skip if there is no .ptl at the end. Otherwise it will remove .mp3
 
 // This code will read all files in the hardcoded directory and remove the extension
 
-const directory = "/Extension-Test";
+// This app needs an interface to be able to select the directory you want.
 
 function readFilesInDirectory(directoryPath) {
-  fs.readdir(path.join(__dirname, directoryPath), (err, files) => {
+  //   reads the files in the directory specified
+  fs.readdir(path.join(homedir, "/desktop", directoryPath), (err, files) => {
     if (err) {
       console.error(`Error reading directory: ${err}`);
       return;
     }
-    console.log(`Files in directory: ${directoryPath}`);
-    files.forEach((file) => {
+    // Filters the files in the directory to only include the ones that have '.ptl' extension
+    const newFiles = files.filter((file) => /\.ptl$/.test(file));
+    console.log("This is newFiles", newFiles);
+    // interates over each file in the array of newFiles and runs removeExtension()
+    newFiles.forEach((file) => {
       removeExtension(file);
     });
   });
 }
 
 const removeExtension = (fileName) => {
+  //   Identifies wha the extension name is for the file
   const fileExtension = path.extname(fileName);
+  //   removes the file extension
   const newFileName = fileName.replace(fileExtension, "");
+  console.log("in removeExtension", fileExtension, newFileName);
+  //   runs the rename function using the old file name and the new file name
   renameFile(fileName, newFileName);
 };
 
 const renameFile = (fileName, newFileName) => {
+  //   rename function using fs()
   fs.rename(fileName, newFileName, async () => {
     console.log("This is fileName", fileName);
     console.log("This is the new file name", newFileName);
   });
-  const oldFilePath = path.join(__dirname, directory, fileName); // specify the current file path
-  const newFilePath = path.join(__dirname, directory, newFileName); // specify the new file path
+  //   The 'directory' should probably be passed in as a parameter
+  const oldFilePath = path.join(homedir, "/desktop", directory, fileName); // specify the current file path
+  const newFilePath = path.join(homedir, "/desktop", directory, newFileName); // specify the new file path
 
   fs.rename(oldFilePath, newFilePath, (err) => {
     if (err) {
@@ -47,12 +66,3 @@ const renameFile = (fileName, newFileName) => {
 };
 
 readFilesInDirectory(directory);
-// removeExtension(file);
-
-const testFolder = "./tests/";
-
-// fs.readdir(testFolder, (err, files) => {
-//   files.forEach((file) => {
-//     console.log(file);
-//   });
-// });
